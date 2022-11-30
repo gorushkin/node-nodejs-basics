8195ebffc40acd37b2fdd5a3750a164c25b6ed0a
gorushkin <gorushkin@gmail.com>
2022-12-01 00:10:33 +0300

    feat: update utils

--- src/fs/utils.js
+++ src/fs/utils.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import fs from 'fs';
+import fs, { promises } from 'fs';
 
 export const getPath = (...args) => {
   const getReccurPath = (list) => {
@@ -15,9 +15,14 @@ export const getAbsolutPath = (...args) => getPath(path.resolve(), getPath.apply
 
 export const checkIfTargetExist = async (target, ifExistError = true) => {
   const errorMessage = 'FS operation failed';
-  const isExist = await fs.existsSync(target);
-  if (ifExistError && isExist) throw new Error(errorMessage);
-  if (!isExist && !ifExistError) throw new Error(errorMessage);
+
+  try {
+    await promises.stat(target);
+    if (ifExistError) throw new Error(errorMessage);
+  } catch (error) {
+    if (error.message === 'FS operation failed') throw new Error(errorMessage);
+    if (error.code === 'ENOENT' && !ifExistError) throw new Error(errorMessage);
+  }
 };
 
 export const checkIfTargetNotExist = async (target) => checkIfTargetExist(target, false);
