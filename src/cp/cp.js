f53e8246711f39e13dea831131d25bb112dfa831
gorushkin <gorushkin@gmail.com>
2022-12-04 20:19:12 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import { getPath, getDirname } from '../fs/utils.js';
+
+const spawnChildProcess = async (args = []) => {
+  const dirname = getDirname(import.meta.url);
+  const filename = 'files/script.js';
+  const filePath = getPath(dirname, filename);
+
+  fork(filePath, args);
 };
 
 spawnChildProcess();
