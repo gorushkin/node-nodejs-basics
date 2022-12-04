6bcdf9a2c593a2c76d119995f7b6ed521e37fd31
gorushkin <gorushkin@gmail.com>
2022-12-05 01:18:20 +0300

    fix: fix arguments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,7 @@
 import { fork } from 'child_process';
 import { getPath, getDirname } from '../fs/utils.js';
 
-const spawnChildProcess = async (args = []) => {
+const spawnChildProcess = async (args) => {
   const dirname = getDirname(import.meta.url);
   const filename = 'files/script.js';
   const filePath = getPath(dirname, filename);
