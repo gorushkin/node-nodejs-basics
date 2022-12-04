2946766598d69d83fe5c7a4a3938a7128a199bf2
gorushkin <gorushkin@gmail.com>
2022-12-04 14:06:59 +0300

    feat: add compress, decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirname, getPath } from '../fs/utils.js';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+  const dirname = getDirname(import.meta.url);
+  const gunzip = createUnzip();
+  const inputFilename = 'archive.gz';
+  const outputFilename = 'fileToCompress.txt';
+  const folder = 'files';
+  const inputPath = getPath(dirname, folder, inputFilename);
+  const outputPath = getPath(dirname, folder, outputFilename);
+
+  const readable = createReadStream(inputPath);
+  const writeable = createWriteStream(outputPath);
+
+  pipeline(readable, gunzip, writeable, (err) => {
+    if (err) console.log(err);
+  });
 };
 
 await decompress();
