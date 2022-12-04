417a7280f924eb575462ddf124dd06f4df8dfadf
gorushkin <gorushkin@gmail.com>
2022-12-04 16:29:37 +0300

    feat: add workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import { getPath, getDirname } from '../fs/utils.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const dirname = getDirname(import.meta.url);
+  const workerFilename = 'worker.js';
+  const workerPath = getPath(dirname, workerFilename);
+  const cpus = os.cpus();
+
+  const runWorker = (n) => {
+    return new Promise((res) => {
+      const worker = new Worker(workerPath, { workerData: n });
+      worker.on('error', () => res({ status: 'error', data: null }));
+      worker.on('message', (data) => res({ status: 'resolved', data }));
+    });
+  };
+
+  const results = await Promise.all(cpus.map((_, i) => runWorker(10 + i)));
+  console.log(results);
 };
 
 await performCalculations();
